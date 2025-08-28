2d6780bd6c14afe09b1687aee953714bd187d8ec
Your Name <k.bristi057@gmail.com>
2025-08-28 09:17:47 +0600

    added the basic setup

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import expressSession from "express-session";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { router } from "./modules/routes";
+const app = express();
+app.use(
+  expressSession({
+    secret: "Your Secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(express.json());
+app.use(cors());
+app.use("/api/v1", router);
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Welcome to Digital Wallet Backend!!",
+  });
+});
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,56 @@
+import { envVars } from "./config/env";
+import { Server } from "http";
+import app from "./app";
+import mongoose from "mongoose";
+let server: Server;
+const startServer = async () => {
+  try {
+    await mongoose.connect(envVars.DB_URL);
+    console.log("Connected to DB!!");
+    server = app.listen(envVars.PORT, () => {
+      console.log(`Server is running on port ${envVars.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+(async () => {
+  await startServer();
+  //await seedSuperAdmin();
+})();
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection detected... Server shutting down", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+    process.exit(1);
+  }
+});
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception detected... Server shutting down", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+    process.exit(1);
+  }
+});
+process.on("SIGTERM", (err) => {
+  console.log("SIGTERM signal received ... Server shutting down", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+    process.exit(1);
+  }
+});
+process.on("SIGINT", (err) => {
+  console.log("SIGINT signal received ... Server shutting down", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+    process.exit(1);
+  }
+});
