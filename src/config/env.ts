0ee8787cca524c98c76c1f6f7b8a1ab93cd8e312
Your Name <k.bristi057@gmail.com>
2025-08-29 16:50:59 +0600

    added the  token and cookie and jwt for login

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,6 +5,12 @@ interface EnvConfig {
   DB_URL: string;
   NODE_ENV: "development" | "production";
   BCRYPT_SALT_ROUND: string;
+  JWT_ACCESS_SECRET: string;
+  JWT_REFRESH_SECRET: string;
+  JWT_ACCESS_EXPIRES: string;
+  JWT_REFRESH_EXPIRES: string;
+  SUPER_ADMIN_EMAIL: string;
+  SUPER_ADMIN_PASSWORD: string;
 }
 const loadEnvVariables = (): EnvConfig => {
   const requiredEnvVariables: string[] = [
@@ -12,6 +18,12 @@ const loadEnvVariables = (): EnvConfig => {
     "DB_URL",
     "NODE_ENV",
     "BCRYPT_SALT_ROUND",
+    "SUPER_ADMIN_PASSWORD",
+    "SUPER_ADMIN_EMAIL",
+    "JWT_REFRESH_EXPIRES",
+    "JWT_ACCESS_EXPIRES",
+    "JWT_REFRESH_SECRET",
+    "JWT_ACCESS_SECRET",
   ];
 
   requiredEnvVariables.forEach((key) => {
@@ -24,6 +36,12 @@ const loadEnvVariables = (): EnvConfig => {
     DB_URL: process.env.DB_URL as string,
     NODE_ENV: process.env.NODE_ENV as "development" | "production",
     BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
+    SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string,
+    SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
+    JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
+    JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
   };
 };
 export const envVars = loadEnvVariables();
